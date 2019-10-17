48f2c676286e2e9cc393261c6d0176a33b860cb7
陳涵宇 <rollr76518@gmail.com>
2019-10-18 01:45:51 +0800

    init

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+	let disposeBag = DisposeBag()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		Observable.just(1)
+			.map({ (number) -> Int in
+				print(Thread.isMainThread)
+				return number
+			})
+			.subscribeOn(MainScheduler.instance)
+			.map({ (number) -> Int in
+				print(Thread.isMainThread)
+				return number
+			})
+			.subscribeOn(MainScheduler.instance)
+			.map({ (number) -> Int in
+				print(Thread.isMainThread)
+				return number
+			})
+			.observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+			.map({ (number) -> String in
+				print(Thread.isMainThread)
+				return "\(number)"
+			})
+			.observeOn(MainScheduler.instance)
+			.subscribe(onNext: { (number) in
+				print(Thread.isMainThread)
+				print(number)
+			})
+			.disposed(by: disposeBag)
 	}
-
-
 }
 
